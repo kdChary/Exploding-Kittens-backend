250d0733f457633ced5e15134b830e114e8c31eb
Siva Dinesh <ksd.ccbp4@gmail.com>
2024-03-16 12:22:50 +0530

    Data base added and port setup successful

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const {open} = require("sqlite");
-const sqlite = require("sqlite3");
+const sqlite3 = require("sqlite3");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+const dataBasePath = path.join(__dirname,"UserData.db");
+let db = null;
+
+const initializeDbAndServer = async () => {
+    try{
+        db = await open({
+            filename : dataBasePath,
+            driver: sqlite3.Database,
+        });
+        app.listen(3000, ()=>console.log(`server running at : https://localhost:3000/` ));
+    } catch (err){
+        console.log(`DB error: ${err.message}`);
+        process.exit(1);
+    }
+};
+
+initializeDbAndServer();
+
